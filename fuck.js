7b535f01e32aa0112969620022cb547842649e3b
jeremykirschner <jakirsch@gmail.com>
2024-01-01 20:13:31 -0500

    Update fuck.js

--- fuck.js
+++ fuck.js
@@ -84,30 +84,30 @@ years.onchange= function(e) {
     outputElm.innerHTML = year+ " Fucking Text Count <br />";
     noerror()
 
-    execute ("select count(*) as 'Sent:'\n from message m inner join handle " +
+    execute ("select count(*) as 'Sent:'\n from message m left join handle " +
         "h on h.ROWID=m.handle_id where strftime('%Y'\n,Date(substr(date,0,10)+strftime('%s'\n,'2001-01-01 00:00:00'\n),'unixepoch'\n))='"+ year +"'\n " +
         "and is_from_me=1;")
 
-    execute ("select count(*) as 'Received:'\n from message m inner join handle " +
+    execute ("select count(*) as 'Received:'\n from message m left join handle " +
         "h on h.ROWID=m.handle_id where strftime('%Y'\n,Date(substr(date,0,10)+strftime('%s'\n,'2001-01-01 00:00:00'\n),'unixepoch'\n))='"+ year +"'\n " +
         "and is_from_me=0;")
     
-    execute ("select count(*) as 'Fucks Received:'\n from message m inner join handle " +
+    execute ("select count(*) as 'Fucks Received:'\n from message m left join handle " +
         "h on h.ROWID=m.handle_id where strftime('%Y'\n,Date(substr(date,0,10)+strftime('%s'\n,'2001-01-01 00:00:00'\n),'unixepoch'\n))='"+ year +"'\n " +
         "and is_from_me=0 " +
         "and text like '%fuck%'\n;")
 
-    execute ("select count(*) as 'Fucks Given:'\n from message m inner join handle " +
+    execute ("select count(*) as 'Fucks Given:'\n from message m left join handle " +
         "h on h.ROWID=m.handle_id where strftime('%Y'\n,Date(substr(date,0,10)+strftime('%s'\n,'2001-01-01 00:00:00'\n),'unixepoch'\n))='"+ year +"'\n " +
         "and is_from_me=1 " +
         "and text like '%fuck%'\n;")
     
-    execute ("select count(*) as 'Love Given:'\n from message m inner join handle " +
+    execute ("select count(*) as 'Love Given:'\n from message m left join handle " +
         "h on h.ROWID=m.handle_id where strftime('%Y'\n,Date(substr(date,0,10)+strftime('%s'\n,'2001-01-01 00:00:00'\n),'unixepoch'\n))='"+ year +"'\n " +
         "and is_from_me=1 " +
         "and (text like '%love you %' or text like '%❤️%' or text like '%💕%' or text like '%😘%' )\n;")
 
-    execute ("select count(*) as 'Love Received:'\n from message m inner join handle " +
+    execute ("select count(*) as 'Love Received:'\n from message m left join handle " +
         "h on h.ROWID=m.handle_id where strftime('%Y'\n,Date(substr(date,0,10)+strftime('%s'\n,'2001-01-01 00:00:00'\n),'unixepoch'\n))='"+ year +"'\n " +
         "and is_from_me=0 " +
         "and (text like '%love you %' or text like '%❤️%' or text like '%💕%' or text like '%😘%' )\n;")
